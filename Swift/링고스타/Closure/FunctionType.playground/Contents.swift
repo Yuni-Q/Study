da018d137c5e10c32a2ddcffa2fa72f92782f0d2
YunHee Lee <carnotcycle2@YunHee-ui-MacBook-Pro.local>
2018-04-19 18:26:01 +0900

    축약

--- Swift/링고스타/Closure/FunctionType.playground/Contents.swift
+++ Swift/링고스타/Closure/FunctionType.playground/Contents.swift
@@ -30,7 +30,33 @@ let price032702 = finalPrice(source: 350.0, additional: couponDiscount)
 let addVATClosure = { (source:Double) -> Double in
     return source * 1.1
 }
+// 생략
+let addVATClosure2 = { source in
+    return source * 1.1
+}
+// 더 생략
+let addVATClosure3 = { source in
+    source * 1.1
+}
+// 위치
+let addVATClosure4 = { $0 * 1.1 }
+
 let couponDiscountClosure = { (source:Double) -> Double in
     return source * 0.9
 }
-let price032703 = addVATClosure(157.6)
+//생략
+let couponDiscountClosure2 = { source in
+    return source * 0.9
+}
+let couponDiscountClosure3 = { source in
+    source * 0.9
+}
+let couponDiscountClosure4 = { $0 * 0.9 }
+var price032703 = addVATClosure(157.6)
+price032703 = addVATClosure2(157.6)
+price032703 = addVATClosure3(157.6)
+price032703 = addVATClosure4(157.6)
+price032703 = couponDiscountClosure(157.6)
+price032703 = couponDiscountClosure2(157.6)
+price032703 = couponDiscountClosure3(157.6)
+price032703 = couponDiscountClosure4(157.6)
