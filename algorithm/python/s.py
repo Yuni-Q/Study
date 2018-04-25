1a1f327e573a5ab734659df8e5f8dae9decc1cbd
YunHee Lee <carnotcycle2@YunHee-ui-MacBook-Pro.local>
2018-04-25 14:47:43 +0900

    algo

---
+++ algorithm/python/s.py
@@ -0,0 +1,20 @@
+def aa(a, b):
+	return a+b
+
+def bb(a, b):
+	return a-b
+
+c = aa(1, 2)
+d = bb(2, 1)
+
+print c
+print d
+
+# 딕셔너리 요소 삭제하기
+# del a[1]
+
+# Key: Value 쌍 모두 지우기(clear)
+# a.clear()
+
+# 해당 Key가 딕셔너리 안에 있는지 조사하기(in)
+# 'name' in a
---
+++ algorithm/python/test.py
@@ -0,0 +1,6 @@
+def aa(a, b):
+	return a+b
+
+c = aa(1, 2)
+
+print c
